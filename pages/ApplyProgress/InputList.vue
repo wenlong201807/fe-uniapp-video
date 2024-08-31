1f49f8e9842c414a39b5f937016381ddcc669452
dragon <1573511441@qq.com>
2024-08-31 19:35:57 +0800

    feat: cell

---
+++ pages/ApplyProgress/InputList.vue
@@ -0,0 +1,45 @@
+<template>
+    <view class="input-list-wrap">
+        <view>asdfasdasdag</view>
+        <ft-cell :list="list" ref="listRef"></ft-cell>
+        <view>asfdasdsadf======</view>
+        <u-button @click="getData">getData</u-button>
+    </view>
+</template>
+
+<script>
+import ftCell from './components/ft-cell.vue'
+export default {
+    name: 'inputListWrap',
+    components: {
+        ftCell,
+    },
+
+    data() {
+        return {
+            list: [
+                {
+                    title: '111',
+                    value: 'sdfasdf',
+                },
+                {
+                    title: '2222',
+                    value: '2222',
+                },
+                {
+                    title: '3333',
+                    value: '333',
+                },
+            ],
+        }
+    },
+
+    methods: {
+        getData() {
+            console.log(this.$refs.listRef.getList())
+        },
+    },
+}
+</script>
+
+<style scoped></style>
---
+++ pages/ApplyProgress/components/ft-cell.vue
@@ -0,0 +1,39 @@
+<template>
+    <u-cell-group>
+        <u-cell :title="item.title" v-for="(item, index) in localList" :key="index">
+            <u--input
+                slot="right-icon"
+                placeholder="请输入内容"
+                v-model="item.value"
+                @change="(e) => change(e, item)"></u--input>
+        </u-cell>
+    </u-cell-group>
+</template>
+
+<script>
+export default {
+    props: {
+        list: {
+            type: Array,
+            default: () => [],
+        },
+    },
+    data() {
+        return {
+            value: '',
+            localList: this.list,
+        }
+    },
+    methods: {
+        getList() {
+            return this.localList
+        },
+        change(e, item) {
+            // console.log('change', e)
+            item.value = e
+            this.emit('change', item)
+        },
+    },
+}
+</script>
+import { props } from 'uview-ui/libs/mixin/mixin';
